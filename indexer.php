07e156fbe91ed7fb965ccc18741219ac240a487b
Abou Haydar Elias <abouhaydar.elias@gmail.com>
2014-05-12 15:31:50 +0200

    update indexer script

--- indexer.php
+++ indexer.php
@@ -1,8 +1,9 @@
 <?php
 require_once("ESIndexer.php");
+require_once("parameters.php");
 
 echo "connecting to ES" . PHP_EOL;
-$es = new ESIndexer();
+$es = new ESIndexer($global_params);
 
 echo "fetching offers" . PHP_EOL;
 $es->set_autocommit_size(10000);
