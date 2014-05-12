b8effbcbcd78d8338a6bf4a804ad266fc22c2240
eliasah <abouhaydar.elias@gmail.com>
2014-05-12 16:39:26 +0200

    Delete parameters.inc.php

--- src/parameters.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace igraal\elasticsearch;
-require_once 'vendor/autoload.php';
-
-
-$global_params = array();
-$global_params['host'] = '192.168.69.15';
-$global_params['port'] = 9200;
